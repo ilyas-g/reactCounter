bc0c9137c3c11c038f96ecc9f0904cc5076ffdd0
Roddoark <ilyas150691@hotmail.fr>
2022-07-20 15:43:33 +0200

    Refactorisation du fichier App.js

--- src/App.js
+++ src/App.js
@@ -6,27 +6,40 @@ function App() {
 
   const [counter, setCounter] = useState(0);
 
+  const buttonPlus = <Button
+    counter={counter}
+    setCounter={setCounter}
+    text="+"
+  />;
+
+  const buttonMinus = <Button
+    counter={counter}
+    setCounter={setCounter}
+    text="-"
+  />;
+
+  console.log("buttonMinus : " + buttonMinus);
   return (
     <div className="App">
-      <Button className={`${counter === 0 ? "d-none" : ""}`}
-        counter={counter}
-        setCounter={setCounter}
-        text="-"
-      />
-      <p>Counter: {counter}</p>
-      <Button className={`${counter === 10 ? "d-none" : ""}`}
-        counter={counter}
-        setCounter={setCounter}
-        text="+"
-      />
-      <br />
-      <br />
-      <Button
-        counter={counter}
-        setCounter={setCounter}
-        text="Reset"
-      />
+      <div>
+        {counter === 0 ? '' : buttonMinus}
+        <p>Counter: {counter}</p>
+        {counter === 10 ? '' : buttonPlus}
 
+        {/* {buttonPlus}
+        {buttonMinus} */}
+        <br />
+        <br />
+        <Button
+          counter={counter}
+          setCounter={setCounter}
+          text="Reset"
+        />
+      </div>
+      {/* <hr />
+      <div>
+        <h1>Ready to Go</h1>
+      </div> */}
     </div >
   );
 }
